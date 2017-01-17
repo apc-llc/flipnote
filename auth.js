85e10e108e4f28f21367ebc7406619a1a5279e8c
Dmitry Mikushin <dmitry@kernelgen.org>
2017-01-17 16:28:20 +0300

    Offloading scripts to separate files

---
+++ auth.js
@@ -0,0 +1,98 @@
+// Your Client ID can be retrieved from your project in the Google
+// Developer Console, https://console.developers.google.com
+var CLIENT_ID = '50950290046-8evmug0398geq2q4nmfgrhn1gojt2mj9.apps.googleusercontent.com';
+
+var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+
+/**
+ * Check if current user has authorized this application.
+ */
+function checkAuth() {
+	gapi.auth.authorize(
+		{
+			'client_id': CLIENT_ID,
+			'scope': SCOPES.join(' '),
+			'immediate': true
+		}, handleAuthResult);
+}
+
+function flip() {
+	var item = document.getElementById("item");
+	item.className += " flipped";
+}
+
+/**
+ * Handle response from authorization server.
+ *
+ * @param {Object} authResult Authorization result.
+ */
+function handleAuthResult(authResult) {
+	var splashDiv = document.getElementById('splash-div');		
+	var loginDiv = document.getElementById('login-div');
+	var onlineDiv = document.getElementById('online-div');
+	if (authResult && !authResult.error) {
+		// Hide auth UI, then load client library.
+		splashDiv.style.background = '#4285f4';
+		onlineDiv.style.display = 'inline';
+		loadDriveApi();
+		setTimeout(flip, 1000);
+	} else {
+		// Show auth UI, allowing the user to initiate authorization by
+		// clicking authorize button.
+		loginDiv.style.display = 'inline';
+	}
+}
+
+/**
+ * Initiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
+function handleAuthClick(event) {
+	gapi.auth.authorize(
+		{client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+		handleAuthResult);
+	return false;
+}
+
+/**
+ * Load Drive API client library.
+ */
+function loadDriveApi() {
+	gapi.client.load('drive', 'v3', listFiles);
+}
+
+/**
+ * Print files.
+ */
+function listFiles() {
+	var request = gapi.client.drive.files.list({
+			'pageSize': 10,
+			'fields': "nextPageToken, files(id, name)"
+		});
+
+		request.execute(function(resp) {
+			appendPre('Files:');
+			var files = resp.files;
+			if (files && files.length > 0) {
+				for (var i = 0; i < files.length; i++) {
+					var file = files[i];
+					appendPre(file.name + ' (' + file.id + ')');
+				}
+			} else {
+				appendPre('No files found.');
+			}
+		});
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+	var pre = document.getElementById('output');
+	var textContent = document.createTextNode(message + '\n');
+	pre.appendChild(textContent);
+}
---
+++ ui.js
@@ -0,0 +1,82 @@
+$(".svg-container").mousedown(function()
+{
+	$(this).css("background-color", "#3f75cf");
+});
+
+$(".svg-container").mouseup(function()
+{
+	$(this).css("background-color", "#4285f4");
+});
+
+$('.svn-container').on('click', function(e)
+{
+	e.stopPropagation();
+});
+
+$(document).on('click', function (e)
+{
+	$(document).find('.svg-container').css("background-color", "#4285f4");
+});
+
+var $timeout = Math.floor(Date.now() / 1000);
+
+$('.flip').swipeleft(function ()
+{
+	if ($(this).find('.item').hasClass('flipped'))
+	{
+	}
+	else
+	{
+		var $newTimeout = Math.floor(Date.now() / 1000);
+
+		if ($newTimeout > $timeout + 1)
+		{	
+			if ($(this).find('.item').hasClass('flipped'))
+			{
+				$(this).find('.item').removeClass('flipped');
+				$(this).find('.input-front').blur();
+			}
+			else
+			{
+				$(this).find('.item').addClass('flipped');
+				$(this).find('.input-back').blur();
+			}
+
+			$timeout = Math.floor(Date.now() / 1000);
+		}
+	}
+});
+
+$('.flip').swiperight(function ()
+{
+	if ($(this).find('.item').hasClass('flipped'))
+	{
+		var $newTimeout = Math.floor(Date.now() / 1000);
+
+		if ($newTimeout > $timeout + 1)
+		{	
+			if ($(this).find('.item').hasClass('flipped'))
+			{
+				$(this).find('.item').removeClass('flipped');
+				$(this).find('.input-front').blur();
+			}
+			else
+			{
+				$(this).find('.item').addClass('flipped');
+				$(this).find('.input-back').blur();
+			}
+
+			$timeout = Math.floor(Date.now() / 1000);
+		}
+	}
+	else
+	{
+	}
+});
+
+$('textarea').each(function () {
+	this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+}).on('input', function () {
+	this.style.height = 'auto';
+	this.style.height = (this.scrollHeight) + 'px';
+});
